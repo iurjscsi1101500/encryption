61ba020ad8dc21f8ad2f5ac6382cbc5841393a21
Atharva Tiwari <atharvatiwari1101@outlook.in>
2025-11-17 16:43:41 +0530

    remove loop
    Signed-off-by: Atharva Tiwari <atharvatiwari1101@outlook.in>

--- encryption.h
+++ encryption.h
@@ -14,29 +14,34 @@ void derive_keys(const uint_fast8_t key_generating_key[32],
 		const uint_fast8_t nonce[12],
 		struct group *ptr)
 {
-	uint_fast32_t counter, i, counter_2;
 	uint_fast8_t block[16], out[16];	
 	AES_KEY aes_key;
 	AES_set_encrypt_key(key_generating_key, 256, &aes_key);
-	for (counter = 0; counter <= 5; counter++) {
-		memset(block, 0, 16);
-		//this will break on BE system (but who uses them anyways)
-		*(uint_fast32_t*)block = counter;
+	memcpy(block + 4, nonce, 12);
 
-		memcpy(block + 4, nonce, 12);
-		AES_encrypt(block, out, &aes_key);
+	*(uint_fast32_t*)block = 0;
+	AES_encrypt(block, out, &aes_key);
+	memcpy(ptr->message_auth_key, out, 8);
 
-		if (counter <= 1) {
-			for (i = 0; i < 8; i++) {
-				ptr->message_auth_key[counter * 8 + i] = out[i];
-			}
-		} else {
-			for (i = 0; i < 8; i++) {
-				ptr->message_encryption_key[counter_2 * 8 + i] = out[i];
-			}
-			counter_2++;
-		}
-	}
+	*(uint_fast32_t*)block = 1;
+	AES_encrypt(block, out, &aes_key);
+	memcpy(ptr->message_auth_key + 8, out, 8);
+
+	*(uint_fast32_t*)block = 2;
+	AES_encrypt(block, out, &aes_key);
+	memcpy(ptr->message_encryption_key, out, 8);
+
+	*(uint_fast32_t*)block = 3;
+	AES_encrypt(block, out, &aes_key);
+	memcpy(ptr->message_encryption_key + 8, out, 8);
+
+	*(uint_fast32_t*)block = 4;
+	AES_encrypt(block, out, &aes_key);
+	memcpy(ptr->message_encryption_key + 16, out, 8);
+
+	*(uint_fast32_t*)block = 5;
+	AES_encrypt(block, out, &aes_key);
+	memcpy(ptr->message_encryption_key + 24, out, 8);
 }
 // openssl already provides AES_CTR (but its absolute dogshit, so i am gonna create my own)
 
