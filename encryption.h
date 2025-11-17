b6a4b94e078f4e955756d2faebbd2d2a276d7a1d
Atharva Tiwari <atharvatiwari1101@outlook.in>
2025-11-17 18:28:14 +0530

    fix AES_CTR
    Signed-off-by: Atharva Tiwari <atharvatiwari1101@outlook.in>

--- encryption.h
+++ encryption.h
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <openssl/aes.h>
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
 struct group {
 	uint_fast8_t message_auth_key[16];
 	uint_fast8_t message_encryption_key[32];
@@ -16,6 +18,7 @@ void derive_keys(const uint_fast8_t key_generating_key[32],
 {
 	uint_fast8_t block[16], out[16];	
 	AES_KEY aes_key;
+
 	AES_set_encrypt_key(key_generating_key, 256, &aes_key);
 	memcpy(block + 4, nonce, 12);
 
@@ -45,24 +48,29 @@ void derive_keys(const uint_fast8_t key_generating_key[32],
 }
 // openssl already provides AES_CTR (but its absolute dogshit, so i am gonna create my own)
 
-void AES_CTR(uint_fast8_t *out, const uint_fast8_t *in, const uint_fast8_t key[32],
-	uint_fast8_t counter_block[16])
+void AES_CTR(uint_fast8_t *out, uint_fast8_t *in, const size_t len, const uint_fast8_t key[32],
+	const uint_fast8_t init_counter_block[16])
 {
 	AES_KEY aes_key;
-	AES_set_encrypt_key(key, 256, &aes_key);
+	uint_fast8_t block[16];
+	uint_fast8_t keystream_block[16];
+	uint_fast32_t ok;
+	size_t i, out_len = 0, local_len = len;
+	uint_fast8_t *local_in = in;
 
-	uint_fast8_t keystream[16];
-	size_t processed = 0, block_size, i;
-	uint_fast32_t counter;
-	while (processed < sizeof(in)) {
-		AES_encrypt(counter_block, keystream, &aes_key);
-		*(uint_fast32_t*)counter_block += 1;
+	memcpy(block, init_counter_block, 16);
+	AES_set_encrypt_key(key, 256, &aes_key);	
+	while(local_len > 0) {
+		AES_encrypt(block, keystream_block, &aes_key);
+		*(uint_fast32_t *)block += 1;
+		ok = MIN(local_len, sizeof(keystream_block));
 
-		block_size = (sizeof(in) - processed > 16) ? 16 : (sizeof(in) - processed);
-		for (i = 0; i < block_size; i++)
-			out[processed + i] = in[processed + i] ^ keystream[i];
+		for (i = 0; i < ok; i++)
+			out[out_len++] = keystream_block[i] ^ local_in[i];
 
-		processed+=block_size;
+		local_in += ok;
+		local_len -= ok;
 	}
+
 }
 #endif
